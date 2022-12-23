0aab7543d1c38d65195769898c49b6be2a5a9d9e
Rupam Sutar <rupamsutar281998@gmail.com>
2022-12-23 12:01:13 +0530

    Extracting Dynamic Parameters value

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,4 +1,11 @@
+import { useRouter } from 'next/router';
+
 const DetailPage = () => {
+    const router = useRouter();
+
+    const newsId = router.query.newsId;
+
+    console.log(newsId);
     return <h1>The Details Page</h1>
 }
 
